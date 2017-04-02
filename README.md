3d24fe0b5a6fba43c4da891f46a9427b575ae393
timtnlee <timtnlee1995@gmail.com>
2017-04-02 11:51:12 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Phone-gap-Cross-Domain
-ajax post cross domain test(phone gap)
+# A Blank PhoneGap App
+
+## Usage
+
+### PhoneGap CLI
+
+    $ phonegap create my-app --template blank
+
+### Desktop
+
+In your browser, open the file:
+
+    /www/index.html
+
---
+++ www/index.js
@@ -0,0 +1,21 @@
+
+            $(function(){
+                $('#btn').click(function(){
+                    $('#demo').append("<p>click</p>");
+                console.log('click');
+                $.ajax({
+                    type:"POST",
+                    url:'http://192.168.11.3:3000/user/test',
+                    data:{name:'tim'},
+                    success: function(data){
+                        $('#demo').append("<p>"+data+"</p>");
+                    },
+                    error: function(xhr, status, error){
+            
+                            alert('err');
+                        
+                    }
+                })
+            })
+            })
+             
